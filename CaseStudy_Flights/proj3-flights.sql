6306da7b100510b9a69d13c114331c86247c7990
MaryanAmir <150050612+MaryanAmir@users.noreply.github.com>
2026-05-21 22:21:29 +0300

    Add files via upload

---
+++ CaseStudy_Flights/proj3-flights.sql
@@ -0,0 +1,15 @@
+#Q1 Find the busiest airport by the number of flights take off
+
+#Q2 Total number of tickets sold per airline
+
+#Q3 List all flights operated by 'IndiGo' with airport names (origin and destination)
+#Q4 For each airport, show the top airline by number of flights departing from there
+#Q5 For each flight, show time taken in hours and categorize it as Short (<2h), Medium (2-5h), or Long (>5h)
+#Q6 Show each passenger s first and last flight dates and number of flights 
+#Q7 Find flights with the highest price ticket sold for each route (origin ? destination)
+
+#Q8 Find the highest spending passenger in each Frequent Flyer Status group
+
+#Q9 Find the total revenue and number of tickets sold for each airline, and rank the airlihes based on total ri
+ 
+#Q10. For each passenger, identify their most frequently used airline. If a passenger has multiple airlines wi-
